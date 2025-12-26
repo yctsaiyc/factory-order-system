77534bc95c3509b46f269446da6105f3b1027880
yctsaiyc <yctsaiyc@gmail.com>
2025-12-26 07:37:50 +0000

    fix: correct Firebase Studio dev.nix previews config via Perplexity

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,26 +1,40 @@
-      # To learn more about how to use Nix to configure your environment
-      # see: https://firebase.google.com/docs/studio/customize-workspace
-      { pkgs, ... }: {
-        # Which nixpkgs channel to use.
-        channel = "stable-24.05"; # or "unstable"
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
 
-        # Use https://search.nixos.org/packages to find packages
-        packages = [
-          # pkgs.go
-          # pkgs.python311
-          # pkgs.python311Packages.pip
-          # pkgs.nodejs_20
-          # pkgs.nodePackages.nodemon
-          pkgs.dotnet-sdk_7
-          pkgs.icu
-        ];
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    # pkgs.go
+    # pkgs.python311
+    # pkgs.python311Packages.pip
+    # pkgs.nodejs_20
+    # pkgs.nodePackages.nodemon
+    pkgs.dotnet-sdk_7
+    pkgs.icu
+  ];
 
-        # Sets environment variables in the workspace
-        env = {};
-        idx = {
-          # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-          extensions = [
-            # "vscodevim.vim"
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+    ];
+
+    # Adding the preview configuration back for you to correct.
+    previews = {
+      enable = true;
+      previews = {
+        web = {
+          command = [
+            "dotnet" "run"
+            "--urls" "http://0.0.0.0:$PORT"
           ];
+          manager = "web";
         };
-      }
+      };
+    };
+  };
+}
